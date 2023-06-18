97f38fbbb5dffe7a66af52f0a43d34dadaff30b2
awar1 <s205826@sggw.edu.pl>
2023-06-18 15:24:45 +0200

    Updates

--- mynotes/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Coctail
-
-class CoctailSerializer(ModelSerializer):
-    class Meta:
-        model = Coctail
-        fields = '__all__'
